437cf6c152a2c99328ed2ad0bce9071239f052b3
Sushil Nepal <nepalsushil000@gmail.com>
2025-12-25 08:32:40 +0545

    Fixe The code

--- circularQueue.c
+++ circularQueue.c
@@ -19,8 +19,22 @@ int isEmpty(Queue *q){
 int isFull(Queue *q){
     return q->size==MAX;
 }
+int dequeue(Queue *q){
+    int value;
+    if(isEmpty(q)){
+        q->canDeque=0;
+        printf("Queue is Empty !CAN NOT DEQUEUE\n");
+        return -1;
+    }
+    value=q->items[q->front];
+    q->front=(q->front+1)%MAX;
+    q->size--;
+    return value;
+}
 void enqueue(Queue *q,int value){
-   
+    if(isFull(q)){
+        dequeue(q);
+    }
     if(isEmpty(q)){
         q->front=0;
     }
@@ -28,24 +42,18 @@ void enqueue(Queue *q,int value){
     q->size++;
     q->items[q->rear]=value;
 }
-int dequeue(Queue *q){
-    int value;
-    if(isEmpty(q)){
-        printf("Queue is Empty !CAN NOT DEQUEUE\n");
-        return -1;
-    }
-    q->canDeque=1;
-    value=q->items[q->front];
-    q->front=(q->front+1)%MAX;
-    return value;
-}
 void dispaly(Queue *q){
     printf("[");
     int i=q->front;
-        while(!isEmpty(q)&&(i-1)!=q->rear){
-            printf("%d,",q->items[i]);
-            i++;
-        }
+    // printf("%d,",q->items[q->front]);
+    // printf("%d,",q->items[q->rear]);
+     if(!isEmpty(q)){
+        while(i!=q->rear) {
+             printf("%d,",q->items[i]);
+             i=(i+1)%MAX;;
+        } 
+        printf("%d",q->items[i]);     
+     }
     printf("]\n");
 }
 int main(){
@@ -56,10 +64,14 @@ int main(){
     enqueue(&q,2);
     enqueue(&q,3);
     enqueue(&q,4);
-    enqueue(&q,5);
     dispaly(&q);
     printf("%d",dequeue(&q));
-    
+    printf("%d",dequeue(&q));
+    dispaly(&q);
+    enqueue(&q,5);
+    enqueue(&q,6);
+    dispaly(&q);
+    enqueue(&q,7);
     dispaly(&q);
     return 0;
 }
